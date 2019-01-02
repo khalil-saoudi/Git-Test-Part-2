8a2f963d9e70864579a4739503af06a7c77ac845
khalil-saoudi <43445510+khalil-saoudi@users.noreply.github.com>
2019-01-02 15:33:39 +0100

    l'ajout du code js au fichier script depuis Github

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 window.onscroll = function() {scrollFunction()};
+// l'ajout du code depuis github 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
